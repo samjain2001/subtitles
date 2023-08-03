3fb48e83311ffbe015f408fdd1522a7cd96bfb34
samjain2001 <141017311+samjain2001@users.noreply.github.com>
2023-08-03 23:38:46 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 echo "imagemagick" >> ~/apt.txt
 echo "imagemagick" >> /etc/environment
-brew install imagemagick
-yum install ImageMagick ImageMagick-perl
 pip install -r requirements.txt
 apt-get update
 apt-get install -y imagemagick
