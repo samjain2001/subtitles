17d2290c53ef9214f5de22e46d553f83e9930dfb
samjain2001 <141017311+samjain2001@users.noreply.github.com>
2023-08-03 15:47:34 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 
 python3 -m venv env
 source env/bin/activate
-pip install -- upgrade pip
-pip install flask
+
+pip install Flask
+pip show Flask
+python /opt/render/project/src/generation.py
+
 pip install -r requirements.txt
