7580e41810027a7273cabc76c55fa2055c01dee1
samjain2001 <141017311+samjain2001@users.noreply.github.com>
2023-08-03 18:44:19 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 echo "imagemagick" >> ~/apt.txt
 echo "imagemagick" >> /etc/environment
 brew install imagemagick
-
+yum install ImageMagick ImageMagick-perl
 pip install -r requirements.txt
 
 pip install Flask
