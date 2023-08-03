d4db23f8c2684e73dc6152c388b8895bf18e1814
samjain2001 <141017311+samjain2001@users.noreply.github.com>
2023-08-03 15:06:52 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,23 +1,11 @@
 #!/bin/bash
 
-# Build script for your project
-
-# Step 1: Build frontend assets
-
-# Step 2: Create and activate virtual environment
-echo "Creating and activating virtual environment..."
+# Create and activate a virtual environment
 python3.9 -m venv venv
 source venv/bin/activate
 
-# Step 3: Install Python dependencies
-echo "Installing Python dependencies..."
-pip install Flask
-
+# Install the dependencies
 pip install -r requirements.txt
-
-
-
-# Step 4: Deactivate virtual environment
-deactivate
-
-echo "Build completed successfully!"
+pip install flask
+# Start your Flask app
+python generation.py
