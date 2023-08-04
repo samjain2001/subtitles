4bdd17a9a2e6f07dc8ee71cfe2a80ba7fc6bdc22
samjain2001 <141017311+samjain2001@users.noreply.github.com>
2023-08-04 09:45:04 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@ echo "imagemagick" >> /etc/environment
 pip install -r requirements.txt
 apt-get update
 apt-get install -y imagemagick
+pip install --upgrade moviepy
 pip install Flask
 pip show Flask
 
