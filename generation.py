b4a8a0916b41bab1974d906c8b8ac175a59eda46
samjain2001 <141017311+samjain2001@users.noreply.github.com>
2023-08-04 16:33:37 +0530

    Update generation.py

--- generation.py
+++ generation.py
@@ -91,7 +91,10 @@ def processFile(filePath):
     video = VideoFileClip(mp4filename)
     subtitles = pysrt.open(srtfilename)
     subtitle_clips = create_subtitle_clips(subtitles, video.size)
-
+    ffmpeg_params = [
+        "-y", "-f", "image2pipe", "-c:v", "png", "-r", "30", "-i", "-", "-c:v", "libx264",
+        "-crf", "18", "-preset", "slow", output_video_file
+    ]
     # Concatenate the subtitle clips with the original video
     final_video = CompositeVideoClip([video] + subtitle_clips, size=video.size).set_audio(video.audio)
 
@@ -115,7 +118,7 @@ def processFile(filePath):
     ffmpeg_logger.addHandler(ffmpeg_handler)
     ffmpeg_params = ["-y", "-f", "image2pipe", "-c:v", "png", "-r", "30", "-i", "-", "-c:v", "libx264", "-crf", "18", "-preset", "slow", output_video_file]
 
-    final_video.write_videofile(output_video_file, codec="libx264", audio_codec="aac", logger=ffmpeg_logger, temp_audiofile="temp-audio.m4a", remove_temp=True, codec_args=ffmpeg_params)
+    final_video.write_videofile(output_video_file, codec="libx264", audio_codec="aac", logger=ffmpeg_logger, temp_audiofile="temp-audio.m4a", remove_temp=True, ffmpeg_params=ffmpeg_params)
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/home', methods=['GET', 'POST'])
